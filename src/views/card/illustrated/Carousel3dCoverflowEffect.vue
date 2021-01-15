3535f5638cb4318dd4ccdaa5de104e25582784f4
898444038@qq.com <898444038@qq.com>
2021-01-15 15:40:02 +0800

    commit

--- src/views/card/illustrated/Carousel3dCoverflowEffect.vue
+++ src/views/card/illustrated/Carousel3dCoverflowEffect.vue
@@ -17,7 +17,7 @@
         <vs-button class="swiper-button-next" radius icon-pack="feather" icon="icon-arrow-right"></vs-button>
         <swiper :options="swiperOption" :dir="$vs.rtl ? 'rtl' : 'ltr'" :key="$vs.rtl">
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-39.jpg" alt="banner"></swiper-slide>
-          <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-35.jpg" alt="banner"></swiper-slide>
+          <!-- <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-35.jpg" alt="banner"></swiper-slide>
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-37.jpg" alt="banner"></swiper-slide>
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-38.jpg" alt="banner"></swiper-slide>
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-33.jpg" alt="banner"></swiper-slide>
@@ -39,7 +39,7 @@
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-33.jpg" alt="banner"></swiper-slide>
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-40.jpg" alt="banner"></swiper-slide>
           <swiper-slide><img class="responsive" src="@/assets/images/pages/carousel/banner-32.jpg" alt="banner"></swiper-slide>
-        
+         -->
           <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
       </div>
@@ -54,11 +54,92 @@
     </vs-row>
 
     <div v-show="isInsert" style="min-heigth:500px">
-      <vs-row vs-type="flex">
-        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input label-placeholder="武将名称" v-model="general.name"/></vs-col>
-        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input label-placeholder="初始等级" v-model="general.level"/></vs-col>
-        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select"><v-select class="flex-end-select" placeholder="请选择父级" v-model="general.parent" :options="[{label: 'Foo', value: 'foo'}]"></v-select></vs-col>
-        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3">insert 4</vs-col>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="武将名称" v-model="general.name"/></vs-col>
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="初始等级" v-model="general.level"/></vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择父级" v-model="general.parent" :options="General4_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择性别" v-model="general.gender" :options="Gender_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="初始武力" v-model="general.force"/></vs-col>
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="初始智力" v-model="general.intellect"/></vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="初始兵力" v-model="general.troops"/></vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择国家" v-model="general.country" :options="Country_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="异化武力" v-model="general.forcex"/></vs-col>
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="异化智力" v-model="general.intellectx"/></vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3"><vs-input class="flex-end-input" label-placeholder="异化兵力" v-model="general.troopsx"/></vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择星级" v-model="general.star" :options="Star_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择兵种类型" v-model="general.arms" :options="Arms_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择武将类型" v-model="general.generalsType" :options="GeneralsType_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择进阶类型" v-model="general.category" :options="Category_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="3" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="请选择命格类型" v-model="general.destinyType" :options="DestinyType_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <!-- <vs-col vs-order="1" vs-type="flex" vs-justify="left" vs-align="left" vs-w="12">武将联协</vs-col> -->
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="right" vs-w="12">
+          <v-select multiple class="flex-end-select" style="width:100%" placeholder="请选择联协" v-model="general.associationList" :options="General4_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="left" vs-align="left" vs-w="12">
+          <vs-button @click="addWeapon" color="danger" type="gradient" icon-pack="feather" icon="icon-plus">新增战器</vs-button>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom" v-for="(item,index) in weaponList" :key="index">
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="5" class="flex-end-row-select">
+          <v-select class="flex-end-select" style="width:95%" placeholder="请选择战器类型" v-model="item.weapon" :options="Weapon_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="5">
+          <vs-input class="flex-end-input" label-placeholder="战器名称" v-model="item.name"/>
+        </vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="left" vs-align="left" vs-w="2" class="flex-end-row-button">
+          <vs-button @click="delWeapon(index)" radius color="danger" type="gradient" icon-pack="feather" icon="icon-minus"></vs-button>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">
+          <vs-input class="flex-end-input" label-placeholder="幻化名称" v-model="general.generalSkin.name"/>
+        </vs-col>
+        <vs-col vs-order="2" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="1号位兵书" v-model="general.armsBook.position1" :options="ArmsPosition_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="2号位兵书" v-model="general.armsBook.position2" :options="ArmsPosition_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="4" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="3号位兵书" v-model="general.armsBook.position3" :options="ArmsPosition_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="5" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="4号位兵书" v-model="general.armsBook.position4" :options="ArmsPosition_list"></v-select>
+        </vs-col>
+        <vs-col vs-order="6" vs-type="flex" vs-justify="center" vs-align="center" vs-w="2" class="flex-end-row-select">
+          <v-select class="flex-end-select" placeholder="5号位兵书" v-model="general.armsBook.position5" :options="ArmsPosition_list"></v-select>
+        </vs-col>
+      </vs-row>
+      <vs-row vs-type="flex" class="row-bottom">
+        <vs-col vs-order="1" vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-button @click="save" class="btn-margin flex-end-button" type="relief" v-show="$acl.check('admin')">新增&保存</vs-button>
+        </vs-col>
       </vs-row>
     </div>
 
@@ -96,9 +177,30 @@ export default {
       isSelect: false,
       isUpdate: false,
       list:[],
+      weaponList:[],
+      dicUrl: '/ppsg/general/getDic',
+      saveUrl: '/ppsg/general/save',
+      General4_list: [],
+      Gender_list: [],
+      Country_list: [],
+      Star_list: [],
+      Arms_list: [],
+      GeneralsType_list: [],
+      Category_list: [],
+      DestinyType_list: [],
+      Weapon_list: [],
+      ArmsPosition_list: [],
       general:{
         name: '',
-        level: 1
+        level: 1,
+        generalSkin: {},
+        armsBook: {
+          // position1: {},
+          // position2: {},
+          // position3: {},
+          // position4: {},
+          // position5: {}
+        }
       },
       swiperOption: {
         autoplay: false, //自动切换
@@ -165,11 +267,58 @@ export default {
     swiper,
     swiperSlide
   },
+  mounted () {
+    this.initData()
+  },
   methods:{
+    getDic (names) {
+      const _this = this
+      _this.$http.post(_this.dicUrl, {dicName: names}).then(response => {
+        if (response.data.code === 1) {
+          //console.log(_this.dicUrl, response)
+          _this.General4_list = response.data.data.General4
+          _this.Gender_list = response.data.data.Gender
+          _this.Country_list = response.data.data.Country
+          _this.Star_list = response.data.data.Star
+          _this.Arms_list = response.data.data.Arms
+          _this.GeneralsType_list = response.data.data.GeneralsType
+          _this.Category_list = response.data.data.Category
+          _this.DestinyType_list = response.data.data.DestinyType
+          _this.Weapon_list = response.data.data.Weapon
+          _this.ArmsPosition_list = response.data.data.ArmsPosition
+        }
+      }).catch(error => { console.log(error) })
+    },
+    initData () {
+      this.general = {
+        name:'陨星庞统', 
+        level:1,
+        parent:{label: '庞统', value: 2013},
+        gender:{label: '男', value:1},
+        force:'111',
+        intellect:'222',
+        troops:'333',
+        country:{label:'蜀', value:2},
+        star:{label:'五星钻卡', value:5},
+        arms:{label:'骑', value:3},
+        generalsType:{label:'天命策士型', value:8},
+        category:{label:'逆命', value:3},
+        destinyType:{label:'逆命3 (330/580/645)', value:4},
+        generalSkin: {},
+        armsBook: {
+          // position1: {},
+          // position2: {},
+          // position3: {},
+          // position4: {},
+          // position5: {}
+        }
+      }
+    },
     insert () {
       this.isInsert = true
       this.isSelect = false
       this.isUpdate = false
+      this.getDic('General4,Gender,Country,Star,Arms,GeneralsType,Category,DestinyType,Weapon,ArmsPosition')
     },
     select () {
       this.isInsert = false
@@ -180,6 +329,83 @@ export default {
       this.isInsert = false
       this.isSelect = false
       this.isUpdate = true
+    },
+    addWeapon () {
+      this.weaponList.push({})
+    },
+    delWeapon (index) {
+      this.weaponList.splice(index, 1)
+    },
+    save () {
+      const _this = this
+      const general = _this.general
+
+      const associationList = general.associationList
+      const association = []
+      if (associationList) {
+        for (let i = 0; i < associationList.length; i++) {
+          association.push({parentId: associationList[i].value, parentName: associationList[i].label, rate: 0.25})
+        }
+      }
+
+      const weapon = []
+      const weaponList = _this.weaponList
+      if (weaponList) {
+        for (let i = 0; i < weaponList.length; i++) {
+          weapon.push({weaponCode: weaponList[i].weapon.value, weaponName: weaponList[i].weapon.label, name: weaponList[i].name})
+        }
+      }
+
+      const data = {
+        name: general.name,
+        level: general.level,
+        parentId: general.parent.value,
+        parentName: general.parent.label,
+        genderCode: general.gender.value,
+        genderName: general.gender.label,
+        force: general.force,
+        intellect: general.intellect,
+        troops: general.troops,
+        countryCode: general.country.value,
+        countryName: general.country.label,
+        forcex: general.forcex,
+        intellectx: general.intellectx,
+        troopsx: general.troopsx,
+        starCode: general.star.value,
+        starName: general.star.label,
+        armsCode: general.arms.value,
+        armsName: general.arms.label,
+        typeCode: general.generalsType.value,
+        typeName: general.generalsType.label,
+        categoryCode: general.category.value,
+        categoryName: general.category.label,
+        destinyCode: general.destinyType.value,
+        destinyName: general.destinyType.label,
+        associationList: association,
+        weaponList: weapon,
+        armsBook: {
+          positionCode1: general.armsBook.position1.value,
+          positionName1: general.armsBook.position1.label,
+          positionCode2: general.armsBook.position2.value,
+          positionName2: general.armsBook.position2.label,
+          positionCode3: general.armsBook.position3.value,
+          positionName3: general.armsBook.position3.label,
+          positionCode4: general.armsBook.position4.value,
+          positionName4: general.armsBook.position4.label,
+          positionCode5: general.armsBook.position5.value,
+          positionName5: general.armsBook.position5.label
+        },
+        generalSkin:{
+          name: general.generalSkin.name
+        }
+      }
+      console.log('data', data)
+      
+      _this.$http.post(_this.saveUrl, data).then(response => {
+        if (response.data.code === 1) {
+          console.log(_this.saveUrl, response)
+        }
+      }).catch(error => { console.log(error) })
     }
   }
 }
@@ -211,7 +437,23 @@ export default {
   .flex-end-row-select{
     align-items: flex-end !important;
   }
+  .flex-end-row-button{
+    align-items: flex-end !important;
+  }
+  .flex-end-input{
+    width: 95%;
+  }
   .flex-end-select{
+    width: 95%;
+  }
+  .flex-end-button{
     width: 100%;
+  }
+  .row-bottom{
+    margin-bottom: 16px;
+  }
+  .button-right{
+    position: absolute;
+    right: 36px;
   }
 </style>
