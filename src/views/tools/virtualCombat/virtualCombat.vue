40558448a9d69169ed81ffa9eeb28bde66dc575b
898444038@qq.com <898444038@qq.com>
2021-01-28 18:44:11 +0800

    commit

--- src/views/tools/virtualCombat/virtualCombat.vue
+++ src/views/tools/virtualCombat/virtualCombat.vue
@@ -3,16 +3,7 @@
 ========================================================================================== -->
 
 <template>
-  <div>
-    <!-- <div id="faq-page">
-      <div class="faq-jumbotron">
-        <div class="faq-jumbotron-content lg:p-32 md:p-24 sm:p-16 p-8 rounded-lg mb-base">
-          <h1 class="mb-1 text-white">虚战力查询</h1>
-          <p class="text-white">Bonbon sesame snaps lemon drops marshmallow ice cream carrot cake croissant wafer.</p>
-          <vs-input placeholder="Search" v-model="faqSearchQuery" icon-pack="feather" icon="icon-search" size="large" class="w-full mt-6" icon-no-border />
-        </div>
-      </div>
-    </div> -->
+  <div class="combat-content">
     <vs-row vs-type="flex" vs-justify="center" style="margin-bottom:30px;">
       <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="2"></vs-col>
       <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="7">
@@ -20,8 +11,172 @@
       </vs-col>
       <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="3">
         <vs-button @click="query" class="btn-margin" size="small" type="relief" style="height: 37px;margin-left: 10px;margin-top: 21px;">查询</vs-button>
+        <vs-button @click="reset" class="btn-margin" size="small" color="warning" type="relief" style="height: 37px;margin-left: 10px;margin-top: 21px;">重置</vs-button>
       </vs-col>
     </vs-row>
+    <div class="vx-row">
+      <div class="vx-col w-full md:w-2/5 lg:w-1/4 rounded-lg">
+        <vx-card>
+          <h4>筛选</h4>
+          <ul class="faq-topics mt-4">
+            <li v-for="category in categories" :key="category.id" class="p-2 font-medium flex items-center" @click="faqFilter = category.id">
+              <div class="h-3 w-3 rounded-full mr-2" :class="'bg-' + category.color"></div><span class="cursor-pointer">{{ category.name }}</span>
+            </li>
+          </ul>
+        </vx-card>
+      </div>
+
+      <!-- FAQ COL -->
+      <div class="vx-col w-full md:w-3/5 lg:w-3/4 mt-12 md:mt-0">
+        <vs-collapse  type="margin" class="p-0">
+          <vs-collapse-item v-for="(que,index) in filteredFaq" style="background-color: #fff;" class="faq-bg rounded-lg" :class="{'mt-0': !index}" :key="que.id">
+            <div slot="header">
+              <h5>
+                <vs-chip class="rank" size="small" color="primary">{{ que.rank }}</vs-chip>
+                <span>{{ que.totalCombat }}</span>
+                <span style="margin-left: 16px;">{{ que.title }}</span>
+              </h5>
+            </div>
+            <p>
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>武将信息</h3></template>
+                <template slot="thead">
+                  <vs-th>武将名称</vs-th>
+                  <vs-th>国家</vs-th>
+                  <vs-th>兵种</vs-th>
+                  <vs-th>性别</vs-th>
+                  <vs-th>总武力</vs-th>
+                  <vs-th>总智力</vs-th>
+                  <vs-th>总兵力</vs-th>
+                  <vs-th>总战力</vs-th>
+                  <vs-th>战意</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                    <vs-td :data="tr.countryName"> {{ tr.countryName }} </vs-td>
+                    <vs-td :data="tr.armsName"> {{ tr.armsName }} </vs-td>
+                    <vs-td :data="tr.genderName"> {{ tr.genderName }} </vs-td>
+                    <vs-td :data="tr.force"> {{ tr.force }} </vs-td>
+                    <vs-td :data="tr.intellect"> {{ tr.intellect }} </vs-td>
+                    <vs-td :data="tr.troops"> {{ tr.troops }} </vs-td>
+                    <vs-td :data="tr.combat"> {{ tr.combat }} </vs-td>
+                    <vs-td></vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>三维属性</h3></template>
+                <template slot="thead">
+                  <vs-th style="width: 80px;">武将名称</vs-th>
+                  <vs-th>基础三维</vs-th>
+                  <vs-th>科技三维</vs-th>
+                  <vs-th>随从三维</vs-th>
+                  <vs-th>四圣石三维</vs-th>
+                  <vs-th>战器三维</vs-th>
+                  <vs-th>兵种三维</vs-th>
+                  <vs-th>将魂三维</vs-th>
+                  <vs-th>兵符三维</vs-th>
+                  <vs-th>战阵三维</vs-th>
+                  <vs-th>战意三维</vs-th>
+                  <vs-th>命格突破</vs-th>
+                  <vs-th>幻化三维</vs-th>
+                  <vs-th>阵法三维</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c">{{ tr.f }}/{{ tr.i }}/{{ tr.t }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c5">{{ tr.f5 }}/{{ tr.i5 }}/{{ tr.t5 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c22">{{ tr.f22 }}/{{ tr.i22 }}/{{ tr.t22 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c6">{{ tr.f6 }}/{{ tr.i6 }}/{{ tr.t6 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c7">{{ tr.f7 }}/{{ tr.i7 }}/{{ tr.t7 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c9">{{ tr.f9 }}/{{ tr.i9 }}/{{ tr.t9 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c15">{{ tr.f15 }}/{{ tr.i15 }}/{{ tr.t15 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c28">{{ tr.f28 }}/{{ tr.i28 }}/{{ tr.t28 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c17">{{ tr.f17 }}/{{ tr.i17 }}/{{ tr.t17 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c21">{{ tr.f21 }}/{{ tr.i21 }}/{{ tr.t21 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c18">{{ tr.f18 }}/{{ tr.i18 }}/{{ tr.t18 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c19">{{ tr.f19 }}/{{ tr.i19 }}/{{ tr.t19 }}</vx-tooltip></vs-td>
+                    <vs-td><vx-tooltip delay=".5s" :text="tr.c20">{{ tr.f20 }}/{{ tr.i20 }}/{{ tr.t20 }}</vx-tooltip></vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>随从</h3></template>
+                <template slot="thead">
+                  <vs-th>武将名称</vs-th>
+                  <vs-th>武随</vs-th>
+                  <vs-th>智随</vs-th>
+                  <vs-th>兵随</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                    <vs-td :data="tr.forceEntourage"> {{ tr.forceEntourage }} </vs-td>
+                    <vs-td :data="tr.intellectEntourage"> {{ tr.intellectEntourage }} </vs-td>
+                    <vs-td :data="tr.troopsEntourage"> {{ tr.troopsEntourage }} </vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>战器</h3></template>
+                <template slot="thead">
+                  <vs-th>武将名称</vs-th>
+                  <vs-th>战器名称</vs-th>
+                  <vs-th>战器基础三维</vs-th>
+                  <vs-th>战器强化三维</vs-th>
+                  <vs-th>战器淬炼三维</vs-th>
+                  <vs-th>战器专属三维</vs-th>
+                  <vs-th>战器淬炼效果1</vs-th>
+                  <vs-th>战器淬炼效果2</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>兵符</h3></template>
+                <template slot="thead">
+                  <vs-th>武将名称</vs-th>
+                  <vs-th>战器名称</vs-th>
+                  <vs-th>战器基础三维</vs-th>
+                  <vs-th>战器强化三维</vs-th>
+                  <vs-th>战器淬炼三维</vs-th>
+                  <vs-th>战器专属三维</vs-th>
+                  <vs-th>战器淬炼效果1</vs-th>
+                  <vs-th>战器淬炼效果2</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+
+              阵法、工坊、被动战力
+              <vs-table :data="que.itemList" search >
+                <template slot="header"><h3>其他</h3></template>
+                <template slot="thead">
+                  <vs-th>武将名称</vs-th>
+                </template>
+                <template slot-scope="{data}">
+                  <vs-tr :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td class="td-center-0" :data="tr.generalNamex"> {{ tr.generalNamex }} </vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+            </p>
+          </vs-collapse-item>
+        </vs-collapse>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,11 +192,62 @@ export default {
       generalUrl: '/ppsg/general/virtualCombat/list',
       queryUrl: '/ppsg/general/virtualCombat/query',
       generalList: [],
-      select_general: []
+      select_general: [],
+      categories: [
+        {
+          id: 1,
+          name: 'All',
+          color: 'grey'
+        },
+        {
+          id: 2,
+          name: 'General',
+          color: 'primary'
+        },
+        {
+          id: 3,
+          name: 'Licenses',
+          color: 'success'
+        },
+        {
+          id: 4,
+          name: 'Company usage',
+          color: 'warning'
+        },
+        {
+          id: 5,
+          name: 'Trademark use',
+          color: 'danger'
+        }
+      ],
+      faqSearchQuery: '',
+      faqFilter: 0,
+      faqs: []
+    }
+  },
+  computed: {
+    filteredFaq () {
+      // return this.faqs.filter((faq) => {
+      //   if (this.faqFilter === 0) {
+      //     return faq.question.toLowerCase().includes('') || faq.ans.toLowerCase().includes('')
+      //   } else if (this.faqFilter === 1) return faq.question.toLowerCase().includes(this.faqSearchQuery.toLowerCase()) || faq.ans.toLowerCase().includes(this.faqSearchQuery.toLowerCase())
+      //   else if (this.faqFilter === 2) return faq.categoryId === 2 && (faq.question.toLowerCase().includes(this.faqSearchQuery.toLowerCase()) || faq.ans.toLowerCase().includes(this.faqSearchQuery.toLowerCase()))
+      //   else if (this.faqFilter === 3) return faq.categoryId === 3 && (faq.question.toLowerCase().includes(this.faqSearchQuery.toLowerCase()) || faq.ans.toLowerCase().includes(this.faqSearchQuery.toLowerCase()))
+      //   else if (this.faqFilter === 4) return faq.categoryId === 4 && (faq.question.toLowerCase().includes(this.faqSearchQuery.toLowerCase()) || faq.ans.toLowerCase().includes(this.faqSearchQuery.toLowerCase()))
+      //   else if (this.faqFilter === 5) return faq.categoryId === 5 && (faq.question.toLowerCase().includes(this.faqSearchQuery.toLowerCase()) || faq.ans.toLowerCase().includes(this.faqSearchQuery.toLowerCase()))
+      // })
+      return this.faqs
     }
   },
   mounted () {
     this.getGeneralList()
+    this.select_general = [
+      {'label': '砺战赵云', 'value':2},
+      {'label': '桀骜孙策', 'value':3},
+      {'label': '飞将吕布', 'value':4},
+      {'label': '灵雎吕姬', 'value':5},
+      {'label': '御甲张辽', 'value':6}
+    ]
   },
   methods:{
     getGeneralList () {
@@ -52,6 +258,10 @@ export default {
           _this.generalList = response.data.data
         }
       }).catch(error => { console.log(error) })
+    },
+    reset () {
+      console.log(JSON.stringify(this.select_general))
+      this.select_general = []
     },
     query () {
       const _this = this
@@ -66,8 +276,10 @@ export default {
       _this.$http.post(_this.queryUrl, params).then(response => {
         console.log('response', response.data.data)
         if (response.data.code === 1) {
+          _this.faqs = response.data.data
           _this.$vs.notify({text: response.data.msg, iconPack: 'feather', icon: 'icon-alert-circle', color: 'success' })
         } else {
+          _this.faqs = []
           _this.$vs.notify({text: response.data.msg, iconPack: 'feather', icon: 'icon-alert-circle', color: 'danger' })
         }
       }).catch(error => { console.log(error) })
@@ -76,6 +288,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.td-center-0 span{
+  width: 80px;
+  display: inline-block;
+}
+</style>
 
+<style scoped>
+.combat-content{
+  height:500px;
+}
+.rank{
+  position: relative;
+  top: -5px;
+  margin-right: 10px;
+}
+.td-center{
+  text-align: center;
+}
+.td-center-0 span{
+  width: 80px;
+  display: inline-block;
+}
 </style>
