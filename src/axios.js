ccc62092d8f69fe5b769c39e9cddbc13f5be24d6
898444038@qq.com <898444038@qq.com>
2021-01-29 19:10:48 +0800

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 // axios
 import axios from 'axios'
 
-const baseURL = 'http://localhost:8088/ivy'
-// const baseURL = 'http://101.37.38.159:91/ivy'
+// 使用axios,axios2未使用
+// const baseURL = 'http://localhost:8088/ivy'
+const baseURL = 'http://101.37.38.159:91/ivy'
 
 export default axios.create({
   baseURL
